051bdf6c7be80b15e827c55be56f3f5d8d3d96b7
diegoarvz4 <darvz.gtz@gmail.com>
2019-07-24 15:32:52 -0500

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,24 @@ class SessionsController < ApplicationController
   
   end
 
-  def create 
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-      log_in user 
-      remember user #creates and encrypts the token and saves it in the database as remember_digest
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
-      #redirect_to user #user_url(user)
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-  end 
+  end
 
   def destroy 
     log_out if logged_in?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
 
   def index 
     @users = User.paginate(page: params[:page])
+    #@users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end 
 
   def show 
     @user = User.find(params[:id])
+    #redirect_to root_url and return unless FILL_IN
   end 
 
   def new
@@ -19,10 +21,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #redirect_to @user 
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to user_url(@user)
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else  
       render 'new'
     end 
