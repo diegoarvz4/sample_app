ddf8091cb167733d59f5a845d32735c214c49814
diegoarvz4 <darvz.gtz@gmail.com>
2019-08-06 23:17:58 -0500

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,8 @@ if Rails.env.production?
         :provider              => 'AWS',
         :aws_access_key_id     => ENV['AKIATZTTGEDBBCII4PYZ'],
         :aws_secret_access_key => ENV['xA0sA8uwlD4usmYzHBzVUJwn15Vo0MnXcl9y8XjL']
-      }
+        :region => ENV['eu-central-1']
+    }
       config.fog_directory     =  ENV['imagemagick-sample-app']
     end
   end
